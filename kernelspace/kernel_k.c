14c5e20a388555a332cd4642c4cfbd159ed406c8
tjx <tangjianxiong@allwinnertech.com>
2020-09-02 11:18:17 +0800

    Increased kernel monitoring of applications

--- kernelspace/kernel_k.c
+++ kernelspace/kernel_k.c
@@ -9,6 +9,10 @@
 #include <net/netlink.h>
 #include <net/sock.h>
 #include <linux/debugfs.h>
+#include <linux/delay.h>
+#include <linux/kthread.h>
+#include <linux/err.h>
+#include <linux/sched.h>
 #define NETLINK_TEST (25)
 #define PID_A (100)
 #define PID_B (101)
@@ -21,14 +25,20 @@
 #define NAME_A 'a'
 #define NAME_B 'b'
 #define NAME_C 'c'
+//thread
+static int get_state_i = 1;
+static struct task_struct *test_task;
+
 static dev_t devId;
 static struct class *cls = NULL;
 struct sock *nl_sk = NULL;
 static u32 normal_num;
 static u32 illegal_num;
-static int connect_sign_b = 0;
-static int connect_sign_c = 0;
+static u32 connect_sign_b = 0;
+static u32 connect_sign_c = 0;
 static struct dentry *root_d;
+/*通过内核线程检测应用程序是否在线*/
+
 static int getconsign(char a)
 {
     int res;
@@ -52,14 +62,14 @@ static int getpid(char a)
     switch (a)
     {
 
-    case 'a':
+    case NAME_A:
         res = PID_A;
         break;
-    case 'b':
+    case NAME_B:
         res = PID_B;
         break;
 
-    case 'c':
+    case NAME_C:
         res = PID_C;
         break;
 
@@ -101,6 +111,9 @@ static void hello_cleanup(void)
     device_destroy(cls, devId);
     class_destroy(cls);
     unregister_chrdev_region(devId, 1);
+    get_state_i = 0;
+    kthread_stop(test_task);
+    test_task = NULL;
 }
 
 static void netlink_send(int pid, uint8_t *message, int len)
@@ -163,37 +176,37 @@ static void netlink_input(struct sk_buff *__skb)
     netlink_send(nlh->nlmsg_pid, kmsg_remsg, sizeof(kmsg_remsg));
     switch (msgtype)
     {
-    // case DATA_CON:
-    //     if (send == NAME_A)
-    //     {
-    //         if ((strcmp(str1, "yes") == 0) && send == NAME_B)
-    //             connect_sign_b = 1;
-    //         if ((strcmp(str1, "yes") == 0) && send == NAME_C)
-    //             connect_sign_c = 1;
-    //     }
-    //     netlink_send(getpid(recv), str1, sizeof(str1));
-    //     break;
+    case DATA_CON:
+        if (send == NAME_A)
+        {
+            if ((str1[0] == 'y') && (recv == NAME_B))
+                connect_sign_b = 1;
+            if ((str1[0] == 'y') && (recv == NAME_C))
+                connect_sign_c = 1;
+        }
+        netlink_send(getpid(recv), str1, sizeof(str1));
+        break;
     default:
         if ((judge(recv) == 1 && judge(send) == 2) || (judge(recv) == 2 && judge(send) == 1))
         {
-            // if (recv == NAME_A)
-            // {
-            //     if (getconsign(send) == 1)
-            //         netlink_send(getpid(recv), str1, sizeof(str1));
-            //     else
-            //     {
-            //         netlink_send(nlh->nlmsg_pid, kmsg_conerr, sizeof(kmsg_conerr));
-            //     }
-            // }
-            // else
-            // {
-            //     if (getconsign(recv) == 1)
-            //         netlink_send(getpid(recv), str1, sizeof(str1));
-            //     else
-            //     {
-            //         netlink_send(nlh->nlmsg_pid, kmsg_conerr, sizeof(kmsg_conerr));
-            //     }
-            // }
+            if (recv == NAME_A)
+            {
+                if (getconsign(send) == 1)
+                    netlink_send(getpid(recv), str1, sizeof(str1));
+                else
+                {
+                    netlink_send(nlh->nlmsg_pid, kmsg_conerr, sizeof(kmsg_conerr));
+                }
+            }
+            else
+            {
+                if (getconsign(recv) == 1)
+                    netlink_send(getpid(recv), str1, sizeof(str1));
+                else
+                {
+                    netlink_send(nlh->nlmsg_pid, kmsg_conerr, sizeof(kmsg_conerr));
+                }
+            }
             netlink_send(getpid(recv), str1, sizeof(str1));
         }
         else
@@ -204,20 +217,53 @@ static void netlink_input(struct sk_buff *__skb)
         break;
     }
 }
+static int thread_get_pstate(void *data)
+{
+    int state_a = 0;
+    int state_b = 0;
+    int state_c = 0;
+    struct task_struct *p;
+    printk(KERN_INFO "Hello,world!\n");
+    while (get_state_i)
+    {
+        state_a = 0;
+        state_b = 0;
+        state_c = 0;
+        for_each_process(p)
+        {
+            if (strcmp(p->comm, "server_a") == 0)
+                state_a = p->state;
+            if (strcmp(p->comm, "client_b") == 0)
+                state_b = p->state;
+            if (strcmp(p->comm, "client_c") == 0)
+                state_c = p->state;
+        }
+        printk(KERN_INFO "\n---------state---------\n");
+        printk(KERN_INFO "the A is in state:[%d]\n", state_a);
+        printk(KERN_INFO "the B is in state:[%d]\n", state_b);
+        printk(KERN_INFO "the C is in state:[%d]\n", state_c);
+        printk(KERN_INFO "---------state---------\n\n");
+        ssleep(10);
+    }
 
+    return 0;
+}
 static __init int netlink_init(void)
 {
     int result;
+    int err;
     struct netlink_kernel_cfg nkc;
     /*create debugfs root directory*/
-    // root_d = debugfs_create_dir("communiation_debugfs", NULL);
-    // if (!root_d)
-    // {
-    //     printk(KERN_INFO "[debugfs]error create root dir\n");
-    //     return 1;
-    // }
-    // debugfs_create_u32("normal_num", 0664, root_d, &normal_num);
-    // debugfs_create_u32("illegal_num", 0664, root_d, &illegal_num);
+    root_d = debugfs_create_dir("communiation_debugfs", NULL);
+    if (!root_d)
+    {
+        printk(KERN_INFO "[debugfs]error create root dir\n");
+        return 1;
+    }
+    debugfs_create_u32("normal_num", 0664, root_d, &normal_num);
+    debugfs_create_u32("illegal_num", 0664, root_d, &illegal_num);
+    debugfs_create_u32("connect_sign_b", 0664, root_d, &connect_sign_b);
+    debugfs_create_u32("connect_sign_c", 0664, root_d, &connect_sign_c);
 
     printk(KERN_WARNING "netlink init start!\n");
     /*注册设备节点*/
@@ -257,7 +303,20 @@ static __init int netlink_init(void)
     }
 
     printk(KERN_ALERT "[netlink]netlink init success!\n");
+    /*create thread to monitor A，B and C*/
+    test_task = kthread_create(thread_get_pstate, NULL, "test_task");
+
+    if (IS_ERR(test_task))
+    {
+        printk("Unable to start kernel thread.\n");
+        err = PTR_ERR(test_task);
+        test_task = NULL;
+        return err;
+    }
+    wake_up_process(test_task);
     return 0;
+    return 0;
+
 err:
     hello_cleanup();
     return -1;
